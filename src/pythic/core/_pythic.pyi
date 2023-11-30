ee3e9561744d700795e81a86b9773d3000f6e98d
cartercanedy <99052281+cartercanedy@users.noreply.github.com>
2023-11-29 19:47:58 -0800

    Update _pythic.pyi

--- src/pythic/core/_pythic.pyi
+++ src/pythic/core/_pythic.pyi
@@ -3,13 +3,9 @@ from typing import (
     Sequence,
     List
 )
-from sys import version_info
 
-if version_info.major == 3 and version_info.minor >= 12:
-    def select[ TIn , TOut ]( frm : Sequence[ TIn ] , how : Callable[ [ TIn ] , TOut ] ) -> List[ TOut ]: ...
-else:
-    from typing import TypeVar
-    _TIn = TypeVar( "_TIn" )
-    _TOut = TypeVar( "_TOut" )
-    
-    def select( frm : Sequence[ _TIn ] , how : Callable[ [ _TIn ] , _TOut ] ) -> List[ _TOut ]: ...
+from typing import TypeVar
+_TIn = TypeVar( "_TIn" )
+_TOut = TypeVar( "_TOut" )
+
+def select( frm : Sequence[ _TIn ] , how : Callable[ [ _TIn ] , _TOut ] ) -> List[ _TOut ]: ...
