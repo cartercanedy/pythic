9a77d92da75dd7134d8ec55de2ddd00ef0fa30c4
cartercanedy <99052281+cartercanedy@users.noreply.github.com>
2023-11-29 19:46:19 -0800

    Update pythic.h

--- src/pythic/_core/pythic.h
+++ src/pythic/_core/pythic.h
@@ -1,3 +1,3 @@
 #define PY_EXC PyExc_Exception
 #define RAISE_MSG_IF( cond , msg ) if ( (cond) ) { if ( !PyErr_Occurred() ) PyErr_SetString( PY_EXC , (msg) ); return NULL; }
-#define RAISE_IF( cond ) RAISE_MSG_IF( cond , "An internal error occurred" )
+#define RAISE_IF( cond ) RAISE_MSG_IF( ( cond ) , "An internal error occurred" )
